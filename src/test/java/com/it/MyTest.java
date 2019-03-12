ce246c56a4d142edc4295c8bab9357c362ec7b44
Ostap Pudlyk <ostap.pudlyk@globallogic.com>
2019-03-12 13:27:16 +0200

    commit1

--- src/test/java/com/it/MyTest.java
+++ src/test/java/com/it/MyTest.java
@@ -1,10 +1,18 @@
 package com.it;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MyTest {
     @Test
     public void test1() {
+        System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://mvnrepository.com");
+        String url = driver.getCurrentUrl();
+        Assert.assertEquals("https://mvnrepository.com/",url);
+        driver.quit();
 
     }
 }
